bc3ccf7c3ba6ae180c91747b31b372b9ea4e73fd
Anubhav2191 <anubhav.gupta2191@gmail.com>
2025-05-12 19:02:29 +0530

    Create Task1.py
    This file contains the task1 solution for the Tutedude python learning

---
+++ Task1.py
@@ -0,0 +1,7 @@
+var1 = int(input('Enter first number: '))
+var2 = int(input('Enter second number: '))
+
+print('Addition : ',var1+var2)
+print('Substraction : ',var1-var2)
+print('Multiplication : ',var1*var2)
+print('Division : ',var1/var2)
